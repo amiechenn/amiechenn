da11ad34b57c929c7d8e6f2c9ad322457c09889a
amiechenn <364011856@qq.com>
2015-05-01 22:58:02 +0800

    d

--- h5/invitation/invitation.js
+++ h5/invitation/invitation.js
@@ -75,8 +75,7 @@ $(".music-box").click(function show(){
 	  return;
 	};
 });
-
- //创建和初始化地图函数：
+  //创建和初始化地图函数：
     function initMap(){
       createMap();//创建地图
       setMapEvent();//设置地图事件
@@ -85,7 +84,7 @@ $(".music-box").click(function show(){
     }
     function createMap(){ 
       map = new BMap.Map("map"); 
-      map.centerAndZoom(new BMap.Point(113.322336,23.095368),17);
+      map.centerAndZoom(new BMap.Point(113.321186,23.095235),16);
     }
     function setMapEvent(){
       map.enableScrollWheelZoom();
