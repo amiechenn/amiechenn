adb2eea010900d002f5cd4fb6d287cf8660a7ae3
amiechenn <364011856@qq.com>
2017-03-15 18:32:15 +0800

    vue

--- domevue/my-porject/webpack.config.js
+++ domevue/my-porject/webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.esm.js',
-            'assets': path.resolve(__dirname, './src/assets'), 
+            'assets': path.resolve(__dirname, './static'), 
         }
     },
     devServer: {//webpack-dev-server配置
